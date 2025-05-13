3073ef0ae3b558ecec01ce44871336a09293a508
hemarajdm <hemarajdm808@gmail.com>
2025-05-13 15:42:40 +0530

    Create information.c

---
+++ information.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int shridevi();
+    printf("name:hemaraj\n");
+    printf("usn:1SV24CS049\n");
+    printf("age:18\n");
+    printf("mobile no:7899343720\n");
+    printf("collage:shridevi institute of engineeriong and technology\n");
+    printf("location:Tumkur\n");
+    return 0;
+    
+}
