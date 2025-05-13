24790b2d63ccb9b24d1a51fb51ceb44b0ae88a91
hemarajdm <hemarajdm808@gmail.com>
2025-05-13 16:29:19 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,temp;
+    printf("enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    temp=a;
+    a=b;
+    b=temp;
+    printf("Swapped:a=%d,b=%d\n",a,b);
