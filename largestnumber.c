be366922c68b42e00977ad50197fe1474f7f9ad5
hemarajdm <hemarajdm808@gmail.com>
2025-05-13 16:03:20 +0530

    Create largestnumber.c

---
+++ largestnumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("%d is a largest number\n",a);
+    else
+    printf("%d is a largest number\n",b);
+}
