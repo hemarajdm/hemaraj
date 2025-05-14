396fe2eaf3bbf654b3b980e5c81f260671cf0f1f
hemarajdm <hemarajdm808@gmail.com>
2025-05-14 22:20:56 +0530

    Create larsest of 3no.c

---
+++ larsest of 3no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int a,b,c;
+int large()
+{
+    if(a>b&&a>c)
+    return a;
+    else if(b>a&&b>c)
+    return b;
+    else
+    return c;
+}
+int main(){
+printf("enter the value of a,b and c:");
+scanf("%d%d%d",&a,&b,&c);
+int result=large(a,b,c);
+printf("%d is large:",result);
+}
