2f494068a9327957972db8d56ce4b541d2d3f8b6
hemarajdm <hemarajdm808@gmail.com>
2025-05-16 10:18:31 +0530

    Create break.c

---
+++ break.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("enter the element:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        if(i==6)
+            break;
+            printf("%d",i);
+            
+    }
+    return 0;
+}
